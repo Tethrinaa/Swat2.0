ee42128b31f420eadc9054508bf583786e5b5aa0
Marcel Munevar <Killeramoeba@gmail.com>
2015-09-22 19:59:24 -0400

    added int requirement functionality

--- game/Swat2.0/scripts/vscripts/itemfunctions.lua
+++ game/Swat2.0/scripts/vscripts/itemfunctions.lua
@@ -39,56 +39,54 @@ function ItemCheck( event )
     local hero = EntIndexToHScript( event.caster_entindex )
     local itemTable = GameMode.ItemInfoKV[itemName]
     print("Checking Restrictions for "..itemName)
-    DeepPrintTable(itemTable)
-
+    --DeepPrintTable(itemTable)
+	
     -- if there is no subtable for this item, end this script
     if itemTable == nil then
 		print("no subtable for this item!")
         return true
     end
-
-    -- This timer is needed because OnEquip triggers before the item actually being in inventory
-    Timers:CreateTimer(0.1,function()
-        -- Go through every item slot
-        for itemSlot = 0, 5, 1 do 
-            local Item = hero:GetItemInSlot( itemSlot )
-            -- When we find the item we want to check
-            if Item ~= nil and itemName == Item:GetName() then
-                DeepPrintTable(Item)
-
-                -- Check Int Restriction
-                if itemTable.intRequired then
-                    print("Name","int Req","Hero Level")
-                    print(itemName,itemTable.intRequired,hero:GetLevel())
-                    -- If the hero doesn't met the level required, show message and disable item
-                    if itemTable.intRequired > hero:GetIntellect() then
-                        FireGameEvent( 'custom_error_show', { player_ID = pID, _error = "You need level "..itemTable.intRequired.." to use this." } )
-                        --DropItem(Item, hero) //need to disable item instead
-                    end 
-                end
-                
-            end
-        end
-    end)
+	
+	-- Check Int Restriction
+	if itemTable.intRequired then
+		--print("Name","int Req","Hero Int")
+		--print(itemName,itemTable.intRequired,hero:GetIntellect())
+		-- If the hero doesn't met the int required
+		if itemTable.intRequired > hero:GetIntellect() then
+					
+		else
+			print("applying data driven modifier")
+			event.ability:ApplyDataDrivenModifier( hero, hero, itemTable.ModifiersAdd, {duration=-1} )
+		end
+	end 
 end
 
-function DropItem( item, hero )
-    -- Error Sound
-    EmitSoundOnClient("General.CastFail_InvalidTarget_Hero", hero:GetPlayerOwner())
+function RemoveItemModifierStats( event )
+	local itemName = event.ability:GetAbilityName()
+    local hero = EntIndexToHScript( event.caster_entindex )
+    local itemTable = GameMode.ItemInfoKV[itemName]
+	
+	-- if there is no subtable for this item, end this script
+    if itemTable == nil then
+		print("no subtable for this item!")
+        return true
+    end	
+	
+	-- Check Int Restriction
+	if itemTable.intRequired then
+		--print("Name","int Req","Hero Int")
+		--print(itemName,itemTable.intRequired,hero:GetIntellect())
+		-- If the hero doesn't met the int required, show message and disable item
+		if itemTable.intRequired > hero:GetIntellect() then
+			--FireGameEvent( 'custom_error_show', { player_ID = pID, _error = "You need int "..itemTable.intRequired.." to use this." } )
+			--DropItem(Item, hero)						
+		else
+			print("removing data driven modifier")
+			event.ability:ApplyDataDrivenModifier( hero, hero, itemTable.ModifiersRemove, {duration=-1} )
+		end
+	end
+		
 
-    -- Create a new empty item
-    local newItem = CreateItem( item:GetName(), nil, nil )
-    newItem:SetPurchaseTime( 0 )
-
-    -- This is needed if you are working with items with charges, uncomment it if so.
-    -- newItem:SetCurrentCharges( goldToDrop )
-
-    -- Make a new item and launch it near the hero
-    local spawnPoint = Vector( 0, 0, 0 )
-    spawnPoint = hero:GetAbsOrigin()
-    local drop = CreateItemOnPositionSync( spawnPoint, newItem )
-    newItem:LaunchLoot( false, 200, 0.75, spawnPoint + RandomVector( RandomFloat( 50, 150 ) ) )
-
-    --finally, remove the item
-    hero:RemoveItem(item)
 end
+
+
