9e6a0a8ee9bb7246fc22392c926ebee1d2807b90
Cazora <rmarchesep@gmail.com>
2015-11-06 11:12:12 -0500

    Added Precache for sounds

--- game/swat2/scripts/vscripts/addon_game_mode.lua
+++ game/swat2/scripts/vscripts/addon_game_mode.lua
@@ -45,6 +45,7 @@ function Precache( context )
     PrecacheUnitByNameSync("npc_dota_hero_venomancer", context)
     PrecacheUnitByNameSync("npc_dota_hero_meepo", context)
     PrecacheUnitByNameSync("npc_dota_hero_batrider", context)
+    PrecacheUnitByNameSync("npc_dota_hero_dark_seer", context)
 end
 
 -- Create the game mode when we activate
