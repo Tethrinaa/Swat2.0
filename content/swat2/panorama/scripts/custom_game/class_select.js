4b0db2b7b6474e29e255870cae65703a485c4c39
Tyler <tylercampbell@mac.com>
2015-11-14 02:34:19 -0500

    Revert "Update class_select.js"
    This reverts commit 12620398aef45459c12cd79adc58676e07ae0af4.

--- content/swat2/panorama/scripts/custom_game/class_select.js
+++ content/swat2/panorama/scripts/custom_game/class_select.js
@@ -4,10 +4,8 @@
 var playerClass = "";
 var playerWeapon = "";
 var playerArmor = "";
-var playerTrait = "";
-var playerSpec = "";
 
-//Called when any button is pressed, routes the call to handlers based on panel (class\armor\weapon\trait\spec)
+//Called when any button is pressed, routes the call to handlers based on panel (class\armor\weapon)
 function Selected(selection, PanelSelected)
 {
    var PanelSelected = $.GetContextPanel().FindChild(PanelSelected);
@@ -17,42 +15,37 @@ function Selected(selection, PanelSelected)
          PanelSelected.visible = false;
          break;
       case "WeaponsPanel":
-         WeaponSelected(selection, PanelSelected);
+         WeaponSelected(selection);
          PanelSelected.visible = false;
-		 break;
+         break;
       case "ArmorPanel":
-         ArmorSelected(selection, PanelSelected); 
+         playerArmor = selection; 
          PanelSelected.visible = false;
-		 break;
-	  case "TraitPanel":
-         TraitSelected(selection, PanelSelected); 
-		 break;
-	  case "TraitPanel2":
-		 TraitSelected(selection, PanelSelected); 
-		 break;
-	  case "SpecPanel":
-         playerSpec = selection; 
-         PanelSelected.visible = false;
-		 break;
+         break;
       case "ConfirmResetPanel":
          ConfirmResetSelected(selection, PanelSelected);
+         break;
    }
    $.Msg(selection);
 	var iPlayerID = Players.GetLocalPlayer();
    return;
 }
 
-// Called when a class button is pressed, pass in the onActivate text and the class panel.  Brings up Weapon Selection
+// Called when a class button is pressed, pass in the onActivate text and the class panel.
 function ClassSelected(selection, PanelSelected)
 {
    playerClass = selection;
    var RootPanel = PanelSelected.GetParent();
    var WeaponsPanel = RootPanel.FindChild("WeaponsPanel");
+   var ArmorPanel = RootPanel.FindChild("ArmorPanel");
+   $.Msg(ArmorPanel);
    WeaponsPanel.visible = true;
-   //Set up the weapon panel based on the class selected.
+   ArmorPanel.visible = true;
+   //Set up the weapons and armor panels based on the class selected.
       switch(selection) {
       case "sniper":
          HideAllWeapons(WeaponsPanel);
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("SniperRifleButton").visible = true;
          break;
       //Silly cyborg has weird armor
@@ -60,17 +53,27 @@ function ClassSelected(selection, PanelSelected)
          HideAllWeapons(WeaponsPanel);
          $.Msg("1");
          WeaponsPanel.FindChild("VindicatorButton").visible = true;
+         ArmorPanel.FindChild("LightArmorButton").visible = false;
+         ArmorPanel.FindChild("MediumArmorButton").visible = false;
+         $.Msg("2")
+         $.Msg(ArmorPanel);
+         $.Msg(ArmorPanel.FindChild("LightArmorButton"));
+         ArmorPanel.FindChild("HeavyArmorButton").visible = false;
+         ArmorPanel.FindChild("AdvancedArmorButton").visible = true;
          break;
       case "demo":
          HideAllWeapons(WeaponsPanel);
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("RocketButton").visible = true;
          break;
       case "medic":
          HideAllWeapons(WeaponsPanel);
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("AssaultRifleButton").visible = true;
          break;
       // Silly maverick can use all the weapons
       case "maverick":
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("AssaultRifleButton").visible = true;
          WeaponsPanel.FindChild("ChaingunButton").visible = true;
          WeaponsPanel.FindChild("VindicatorButton").visible = false;
@@ -80,14 +83,17 @@ function ClassSelected(selection, PanelSelected)
          break;
       case "ho":
          HideAllWeapons(WeaponsPanel);
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("ChaingunButton").visible = true;
          break;
       case "psychologist":
          HideAllWeapons(WeaponsPanel);
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("AssaultRifleButton").visible = true;
          break;
       case "tactician":
          HideAllWeapons(WeaponsPanel);
+         ShowStandardArmor(ArmorPanel);
          WeaponsPanel.FindChild("AssaultRifleButton").visible = true;
          break;
    }
@@ -104,8 +110,7 @@ function HideAllWeapons(Panel)
    Panel.FindChild("SniperRifleButton").visible = false;
 }
 
-//Assigns correct weapon types and opens up armor selection
-function WeaponSelected(selection, PanelSelected)
+function WeaponSelected(selection)
 {
    
    playerWeapon = selection;
@@ -125,118 +130,8 @@ function WeaponSelected(selection, PanelSelected)
       playerWeapon = "assault_rifle_urban";
    }
    $.Msg(playerWeapon);
-   
-   var RootPanel = PanelSelected.GetParent();
-   var ArmorPanel = RootPanel.FindChild("ArmorPanel");
-   $.Msg(ArmorPanel);
-   ArmorPanel.visible = true;
-   
-   switch(playerClass) {
-      case "sniper":
-         ShowStandardArmor(ArmorPanel);
-         break;
-      //Silly cyborg has weird armor
-      case "cyborg":
-         $.Msg("2")
-         $.Msg(ArmorPanel);
-         $.Msg(ArmorPanel.FindChild("LightArmorButton"));
-         ArmorPanel.FindChild("HeavyArmorButton").visible = false;
-         ArmorPanel.FindChild("AdvancedArmorButton").visible = true;
-         break;
-      case "demo":
-         ShowStandardArmor(ArmorPanel);
-         break;
-      case "medic":
-         ShowStandardArmor(ArmorPanel);
-         break;
-      // Silly maverick can use all the weapons
-      case "maverick":
-         ShowStandardArmor(ArmorPanel);
-         break;
-      case "ho":
-         ShowStandardArmor(ArmorPanel);
-         break;
-      case "psychologist":
-         ShowStandardArmor(ArmorPanel);
-         break;
-      case "tactician":
-         ShowStandardArmor(ArmorPanel);
-         break;
-		}
-		$.Msg(playerWeapon);
 }
 
-//Armor was selected so now open up the traits selection
-function ArmorSelected(selection, PanelSelected) {
-	
-	playerArmor = selection;
-	$.Msg(playerArmor);
-	
-   var RootPanel = PanelSelected.GetParent();
-   var TraitPanel = RootPanel.FindChild("TraitPanel");
-   var TraitPanel2 = RootPanel.FindChild("TraitPanel2");
-   $.Msg(TraitPanel);
-   
-   TraitPanel.visible = true;
-   TraitPanel2.visible = true;
-   
-   TraitPanel.FindChild("SkilledButton").visible = true;
-   TraitPanel.FindChild("SurvivalistButton").visible = true;
-   TraitPanel.FindChild("AcrobatButton").visible = true;
-   TraitPanel.FindChild("HealerButton").visible = true;
-   TraitPanel.FindChild("ChemreliantButton").visible = true;
-   TraitPanel.FindChild("GadgeteerButton").visible = true;
-   TraitPanel.FindChild("EnergizerButton").visible = true;
-   TraitPanel.FindChild("EngineerButton").visible = true;
-   TraitPanel2.FindChild("GiftedButton").visible = true;
-   TraitPanel2.FindChild("DragoonButton").visible = true;
-   TraitPanel2.FindChild("SwiftlearnerButton").visible = true;
-   TraitPanel2.FindChild("FlowerchildButton").visible = true;
-   TraitPanel2.FindChild("RadresistantButton").visible = true;
-   TraitPanel2.FindChild("ProwlerButton").visible = true;
-   TraitPanel2.FindChild("PackratButton").visible = true;
-   TraitPanel2.FindChild("RecklessButton").visible = true;
-}
-
-//Trait was selected so now open up the Specs selection
-function TraitSelected(selection, PanelSelected) {
-	
-	playerTrait = selection;
-	$.Msg(playerTrait);
-	
-   var RootPanel = PanelSelected.GetParent();
-   var SpecPanel = RootPanel.FindChild("SpecPanel");
-   var TraitPanel = RootPanel.FindChild("TraitPanel");
-   var TraitPanel2 = RootPanel.FindChild("TraitPanel2");
-   TraitPanel.visible = false;
-   TraitPanel2.visible = false;
-   
-   $.Msg(SpecPanel);
-   
-   SpecPanel.visible = true;
-   
-   SpecPanel.FindChild("WeaponryButton").visible = true;
-   SpecPanel.FindChild("EnergyCellsButton").visible = true;
-   SpecPanel.FindChild("TriageButton").visible = true;
-   SpecPanel.FindChild("LeadershipButton").visible = true;
-   SpecPanel.FindChild("PowerArmorButton").visible = true;
-   SpecPanel.FindChild("CyberneticsButton").visible = true;
-   SpecPanel.FindChild("ChemistryButton").visible = true;
-   SpecPanel.FindChild("RoboticsButton").visible = true;
-   
-   //Check if sniper was selected
-   switch(playerClass) {
-	   
-	   case "sniper":
-	   SpecPanel.FindChild("Espionage2Button").visible = true;
-	   SpecPanel.FindChild("EspionageButton").visible = false;
-	   break;
-	   default:
-	   SpecPanel.FindChild("EspionageButton").visible = true;
-	   SpecPanel.FindChild("Espionage2Button").visible = false;
-	   break;
-   }
-}
 // Sets up the armor panel for everyone but the cyborg
 function ShowStandardArmor(APanel)
 {
@@ -267,9 +162,6 @@ function ConfirmResetSelected(selection, Panel)
          RootPanel.FindChild("ClassesPanel").visible = true;
          RootPanel.FindChild("WeaponsPanel").visible = false;
          RootPanel.FindChild("ArmorPanel").visible = false;
-		 RootPanel.FindChild("TraitPanel").visible = false;
-		 RootPanel.FindChild("TraitPanel2").visible = false;
-		 RootPanel.FindChild("SpecPanel").visible = false;
          break;
    }
 }
