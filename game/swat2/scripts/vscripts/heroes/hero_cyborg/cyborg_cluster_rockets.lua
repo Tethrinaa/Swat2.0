17af7aec54d142f79f9969d7a7cabb53291fff35
NSEnigma <NmdSnprEnigma@gmail.com>
2015-11-14 00:43:24 -0500

    Parameterized cluster rockets better.

--- game/swat2/scripts/vscripts/heroes/hero_cyborg/cyborg_cluster_rockets.lua
+++ game/swat2/scripts/vscripts/heroes/hero_cyborg/cyborg_cluster_rockets.lua
@@ -1,13 +1,23 @@
 -- Author: NSEnigma
 
+--- A single instance of a cluster tick.
+-- Tick count and interval controlled by a datadriven thinker
 function ClusterThink(keys)
+
 	local radius = keys.ability:GetSpecialValueFor("radius")
-	local units = FindUnitsInRadius(DOTA_TEAM_GOODGUYS, keys.target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, FIND_ANY_ORDER, false)
+	local team =  keys.ability:GetAbilityTargetTeam()
+	local Type =  keys.ability:GetAbilityTargetType()
+	local flags = keys.ability:GetAbilityTargetFlags()
+	local damage_type = keys.ability:GetAbilityDamageType()
+
+	-- Grab all the units in cluster rockets target area	
+	local units = FindUnitsInRadius(keys.caster:GetTeamNumber(), keys.target:GetAbsOrigin(), nil, radius, team, Type, flags, FIND_ANY_ORDER, false)
 	local count = #units
 	if count < 1 then
 		return
 	end
 	
+	-- Split the damage over all the units
 	local total_damage_cap =  keys.ability:GetSpecialValueFor("total_damage_cap")
 	local unit_damage_cap = keys.ability:GetSpecialValueFor("unit_damage_cap")
 	local damage = total_damage_cap / count
@@ -15,19 +25,23 @@ function ClusterThink(keys)
 		damage = unit_damage_cap
 	end
 	damage = damage / keys.ability:GetSpecialValueFor("ticks")
+	
+	
 	for _,unit in pairs(units) do
 		if unit ~= nil then
+		
+			-- Set the stun duration so they all end at the same time regardless of when they walk into the area
 			local thinker_modifier = keys.target:FindModifierByName("modifier_cyborg_cluster_rockets_thinker")
 			local stun_time = keys.ability:GetSpecialValueFor("stun_duration")
 			local stun_remaining = stun_time - thinker_modifier:GetElapsedTime()
-			
 			unit:AddNewModifier( keys.target, keys.ability, "modifier_stunned", { duration = stun_remaining} )
-						
+			
+			-- Deal the split damage
 			local damage_table = {
 				victim = unit,
 				attacker = keys.caster,
 				damage = damage,
-				damage_type = DAMAGE_TYPE_PURE,
+				damage_type = damage_type,
 				ability = keys.ability
 			}
 			ApplyDamage( damage_table )
