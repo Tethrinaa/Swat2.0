8916bf6735b6e5b2e725f9789ceb91219cf3ffc2
Tyler <tylercampbell@mac.com>
2015-12-08 23:35:22 -0500

    Added in Beastlings

--- game/swat2/scripts/vscripts/game/minions/BeastSpawner.lua
+++ game/swat2/scripts/vscripts/game/minions/BeastSpawner.lua
@@ -7,6 +7,7 @@ BeastSpawner = {}
 
 BeastSpawner.BEAST_UNIT_NAME = "enemy_minion_beast"
 BeastSpawner.BEAST_RADINATING_UNIT_NAME = "enemy_minion_beast_radinating"
+BeastSpawner.BEASTLING_UNIT_NAME = "enemy_minion_beastling"
 
 function BeastSpawner:new(o)
     o = o or {}
@@ -47,8 +48,7 @@ function BeastSpawner:createNormal(position)
     unit:SetMana(0)
 
     -- EnemySpawner will look for onDeathFunctions and call them
-    -- TODO: Use for beastlings
-    --unit.onDeathFunction = function(killedUnit, killerEntity, killerAbility) self:onDeath(killedUnit, killerEntity, killerAbility) end
+    unit.onDeathFunction = function(killedUnit, killerEntity, killerAbility) self:onDeath(killedUnit, killerEntity, killerAbility) end
 
     return unit
 end
@@ -106,7 +106,88 @@ function BeastSpawner:createRadinating(position)
     end
 end
 
+-- Spawns beastling at the provided location
+function BeastSpawner:spawnBeastling(position)
+    local unit = CreateUnitByName( BeastSpawner.BEASTLING_UNIT_NAME, position, true, nil, nil, DOTA_TEAM_BADGUYS )
+
+    unit:SetMana(1) -- One blink initially
+
+    -- Apply enemy upgrades
+    g_EnemyUpgrades:upgradeMob(unit)
+
+    -- Set its speed
+    unit:SetBaseMoveSpeed(g_EnemyUpgrades:calculateMovespeed(unit, 24 / g_GameManager.difficultyBase))
+
+    -- Increase minion count
+    g_EnemySpawner.minionCount = g_EnemySpawner.minionCount + 1
+
+    -- Apply an experience value to the unit, if one was not already set in the spawner
+    -- This value will be retrieved from the npc_units_custom file under the key SwatXP
+    if not unit.experience then
+        local unit_info = GameMode.unit_infos[unit:GetUnitName()]
+        if unit_info then
+            local experience = unit_info["SwatXP"]
+            if experience then
+                unit.experience = experience
+            end
+        end
+    end
+
+    return unit
+end
+
+
+-- Spawns flaming beastling at the provided location
+function BeastSpawner:spawnFlamingBeastling(position)
+    local unit = self:spawnBeastling(position)
+
+    unit:AddAbility("enemy_common_burninating")
+    unit:FindAbilityByName("enemy_common_burninating"):SetLevel(1)
+
+    return unit
+end
+
 -- Called when this unit dies
 function BeastSpawner:onDeath(killedUnit, killerEntity, killerAbility)
-    -- TODO: Beastlings
+    -- Spawns beastling if we have enough mana
+    local numberToSpawn = math.floor(killedUnit:GetMana() / 11.2)
+
+    -- TODO: Check to see if this beast died by something like an ion strike or something and don't spawn beastlings
+
+    if numberToSpawn > 0 then
+        local applyFlaming = (killedUnit:FindAbilityByName("enemy_common_burninating") ~= nil)
+        if SHOW_BEAST_LOGS then
+            print("BeastSpawner | Beast died. Spawning " .. numberToSpawn .. " beastlings! (fire= " .. tostring(applyFlaming) .. ")")
+        end
+
+        local position = killedUnit:GetAbsOrigin()
+
+        Timers:CreateTimer(RandomFloat(0, 3), function()
+            local unit = nil
+            if applyFlaming then
+                unit = self:spawnFlamingBeastling(position)
+            else
+                unit = self:spawnBeastling(position)
+            end
+
+            if killerEntity then
+                -- Send it after whoever killed it!
+                -- Issueing an order to a unit immediately after it spawns seems to not consistently work
+                -- so we'll wait a second before telling the group where to go
+                Timers:CreateTimer(0.5, function()
+                    g_EnemyCommander:doMobAction(unit, killerEntity)
+                end)
+            end
+
+            numberToSpawn = numberToSpawn - 1
+            if numberToSpawn > 0 then
+                return RandomFloat(0, 3)
+            end
+        end)
+    else
+        -- Not enough to mana to create any
+        if SHOW_BEAST_LOGS then
+            print("BeastSpawner | Beast died. But no beastlings will spawn")
+        end
+    end
 end
